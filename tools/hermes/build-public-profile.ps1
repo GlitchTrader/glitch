ea588048d669b8d28c996046ba626f224fc27aac
architectonic <ab@alanborger.com>
2026-07-24 16:37:13 -0300

    release(hermes): prepare profile v0.0.2.13

--- tools/hermes/build-public-profile.ps1
+++ tools/hermes/build-public-profile.ps1
@@ -103,7 +103,7 @@ if ($unexpectedAfter.Count -gt 0) { throw "Public profile gained unexpected path
 [ordered]@{
     schema_version = 'glitch.hermes.public_profile_build.v1'
     target = $target
-    version = '0.0.2.12'
+    version = '0.0.2.13'
     skills = $skillCount
     scripts = $scriptCount
     files = $textFiles.Count + 1
